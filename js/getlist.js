49d1fc33c432717a1df3eba80b50f53cae153b1c
roman <gosahorosij328@gmail.com>
2023-06-09 10:29:02 +0300

    прогнал через валидатор (ошибки по мелочи)

--- js/getlist.js
+++ js/getlist.js
@@ -4,16 +4,12 @@ xhr.open('POST', 'https://jscp-diplom.netoserver.ru/');
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 xhr.responseType = 'json';
 xhr.send('event=update');
-
 xhr.addEventListener('load', () => {
-    const responseObj = xhr.response;
     const films = xhr.response.films;
     const halls = xhr.response.halls;
     const seances = xhr.response.seances;
-    console.log(responseObj)
-
     films.result.forEach((el) => {
-          main.insertAdjacentHTML("afterBegin", `
+        main.insertAdjacentHTML("afterBegin", `
           <section class="movie" data-movieId="${el.film_id}">
               <div class="movie__info">
               <div class="movie__poster">
@@ -28,38 +24,32 @@ xhr.addEventListener('load', () => {
                 </p>
               </div>
             </div> 
-          </section>`)  
+          </section>`);
     });
-
-    const movieId = Array.from(document.querySelectorAll('.movie')); 
-    
+    const movieId = Array.from(document.querySelectorAll('.movie'));
     movieId.forEach((element) => {
-       halls.result.forEach((elem) => {
-          if(Number(elem.hall_open) != 0) {
-            element.insertAdjacentHTML("beforeEnd", `
+        halls.result.forEach((elem) => {
+            if (Number(elem.hall_open) !== 0) {
+                element.insertAdjacentHTML("beforeEnd", `
                <div class="movie-seances__hall movie-seances__hall__remove" data-hallId="${elem.hall_id}">
                   <h3 class="movie-seances__hall-title">${elem.hall_name}</h3>
                   <ul class="movie-seances__list"></ul>
                </div>
-            `)
-          }
-       });
-    });   
-
+            `);
+            }
+        });
+    });
     const movieSeancesList = Array.from(document.querySelectorAll('.movie-seances__list'));
-
     movieSeancesList.forEach((e) => {
-       const dataMovieId = e.closest(".movie").getAttribute('data-movieId');
-       const movieSeances = e.parentElement.getAttribute('data-hallId');
-    //    console.log(`${movieSeances} <--> ${dataMovieId}`)
-       
-       for(let i = 0; i < seances.result.length; i++) {
-          if(dataMovieId === seances.result[i].seance_filmid && movieSeances === seances.result[i].seance_hallid) {
-              e.parentElement.classList.add('movie-seances__hall__add');
-              e.insertAdjacentHTML("beforeEnd", `
+        const dataMovieId = e.closest(".movie").getAttribute('data-movieId');
+        const movieSeances = e.parentElement.getAttribute('data-hallId');
+        for (let i = 0; i < seances.result.length; i++) {
+            if (dataMovieId === seances.result[i].seance_filmid && movieSeances === seances.result[i].seance_hallid) {
+                e.parentElement.classList.add('movie-seances__hall__add');
+                e.insertAdjacentHTML("beforeEnd", `
               <li class="movie-seances__time-block"><a class="movie-seances__time" href="hall.html">${seances.result[i].seance_time}<a></li>
               `);
-          };     
-       };   
+            }
+        }
     });
 });
